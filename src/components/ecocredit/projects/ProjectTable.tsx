de049932a8c1aed69395deee740c21d686333034
Aaron Craelius <aaronc@users.noreply.github.com>
2024-04-02 23:15:35 -0400

    WIP

--- src/components/ecocredit/projects/ProjectTable.tsx
+++ src/components/ecocredit/projects/ProjectTable.tsx
@@ -2,23 +2,41 @@ import {
     QueryProjectsResponseSDKType
 } from "../../../../../../regen-js/packages/api/types/codegen/regen/ecocredit/v1/query";
 import {Cell, Column, Row, Table, TableBody, TableHeader} from "react-aria-components";
+import Link from "next/link";
+import {
+    PageResponseSDKType
+} from "../../../../../../regen-js/packages/api/src/codegen/cosmos/base/query/v1beta1/pagination";
+import {ProjectInfoSDKType} from "../../../../../../regen-js/packages/api/src/codegen/regen/ecocredit/v1/query";
 
-export type ProjectTableProps = QueryProjectsResponseSDKType & {};
+export type ProjectTableProps = {
+    projects: ProjectInfoSDKType[];
+    pagination?: PageResponseSDKType;
+};
 
 export const ProjectTable = ({projects}: ProjectTableProps) => {
     return (
         <div>
             <Table>
                 <TableHeader>
-                    <Column>ID</Column>
+                    <Column isRowHeader>ID</Column>
+                    <Column>Credit Class</Column>
+                    <Column>Jurisdiction</Column>
+                    <Column>Admin</Column>
                 </TableHeader>
                 <TableBody>
                     {projects.map((project) => (
-                        <Row key={project.id}>
-                            <Cell>{project.id}</Cell>
+                        <Row key={project.id} className="border">
+                            <TableCell><Link href={`/ecocredit/projects/${project.id}`}>{project.id}</Link></TableCell>
+                            <TableCell><Link href={`/ecocredit/classes/${project.class_id}`}>{project.class_id}</Link></TableCell>
+                            <TableCell>{project.jurisdiction}</TableCell>
+                            <TableCell>{project.admin}</TableCell>
                         </Row>))}
                 </TableBody>
             </Table>
         </div>
     );
+}
+
+const TableCell = ({children}: { children: React.ReactNode }) => {
+    return <Cell className="px-4 py-1 border">{children}</Cell>;
 }
